6a58bae9abf40864a811ff190fce70bab06bf2e7
kadoya-cyclin <s.kondou@cyclin.jp>
2012-12-20 16:53:37 +0900

    NCMenuItemClass is over

--- NewsCubeMenu/NewsCubeMenu/NCMenuItem.h
+++ NewsCubeMenu/NewsCubeMenu/NCMenuItem.h
@@ -8,6 +8,20 @@
 
 #import <UIKit/UIKit.h>
 
+@protocol NCMenuItemDelegate;
+
 @interface NCMenuItem : UIImageView
 
+@property(nonatomic, strong) UIImageView *contentImageView;
+
+@property(nonatomic, weak) id <NCMenuItemDelegate> delegate;
+
+// Create MenuItem
+-(id)initWithImage:(UIImage *)image highlightedImage:(UIImage *)hImage ContentImage:(UIImage *)cImage highlightedContentImage:(UIImage *)hcImage;
+
+@end
+
+@protocol NCMenuItemDelegate <NSObject>
+-(void)menuItemTouchesBegan:(NCMenuItem *)menuItem;
+-(void)menuItemTouchesEnd:(NCMenuItem *)menuItem;
 @end
--- NewsCubeMenu/NewsCubeMenu/NCMenuItem.m
+++ NewsCubeMenu/NewsCubeMenu/NCMenuItem.m
@@ -8,24 +8,76 @@
 
 #import "NCMenuItem.h"
 
+static inline CGRect ScaleRect(CGRect rect, float n){
+    return CGRectMake((rect.size.width - rect.size.width * n)/2, (rect.size.height - rect.size.height * n)/2, rect.size.width * n, rect.size.height * n);
+}
+
 @implementation NCMenuItem
 
-- (id)initWithFrame:(CGRect)frame
-{
-    self = [super initWithFrame:frame];
+#pragma mark -- Initialization And CreanUp --
+-(id)initWithImage:(UIImage *)image highlightedImage:(UIImage *)hImage ContentImage:(UIImage *)cImage highlightedContentImage:(UIImage *)hcImage{
+    self = [super init];
     if (self) {
-        // Initialization code
+        return nil;
     }
+    
+    // Initialization Code Here...
+    self.image = image;
+    self.highlightedImage = hImage;
+    self.userInteractionEnabled = YES;
+    _contentImageView = [[UIImageView alloc] initWithImage:cImage];
+    _contentImageView.highlightedImage = hcImage;
+    [self addSubview:_contentImageView];
+    
     return self;
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
+#pragma mark -- UIView's Method --
+-(void)layoutSubviews{
+    [super layoutSubviews];
+    
+    self.bounds = CGRectMake(0, 0, self.image.size.height, self.image.size.width);
+    float width = _contentImageView.image.size.width;
+    float height = _contentImageView.image.size.height;
+    
+    _contentImageView.frame = CGRectMake(self.bounds.size.width/2 - width/2, self.bounds.size.height/2 - height/2, width, height);
 }
-*/
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    self.highlighted = YES;
+    
+    if ([_delegate respondsToSelector:@selector(menuItemTouchesBegan:)]) {
+        [_delegate menuItemTouchesBegan:self];
+    }
+}
+
+-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event{
+    // if move out of 2x rect, cancel select menu...
+    CGPoint location = [[touches anyObject] locationInView:self];
+    if (!CGRectContainsPoint(ScaleRect(self.bounds, 2.0f), location)) {
+        self.highlighted = NO;
+    }
+}
+
+-(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event{
+    self.highlighted = NO;
+    CGPoint location = [[touches anyObject] locationInView:self];
+    if (CGRectContainsPoint(ScaleRect(self.bounds, 2.0f), location)) {
+        if ([_delegate respondsToSelector:@selector(menuItemTouchesEnd:)]) {
+            [_delegate menuItemTouchesEnd:self];
+        }
+    }
+}
+
+-(void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event{
+    self.highlighted = NO;
+}
+
+#pragma mark -- ContentImage Highlight Method --
+-(void)setHighlighted:(BOOL)highlighted{
+    [super setHighlighted:highlighted];
+    
+    [_contentImageView setHighlighted:highlighted];
+}
 
 @end
